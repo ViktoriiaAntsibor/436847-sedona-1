8dbd1019fb154491d6252090004798531cd8d6f2
Viktoriia Antsibor <30809476+ViktoriiaAntsibor@users.noreply.github.com>
2017-11-22 10:39:13 +0300

    22.11.17
    final

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var checkbox = document.querySelector(".search-checkbox");
+var formhide = document.querySelector(".form");
+
+checkbox.addEventListener("click", function (evt) {
+formhide.classList.toggle("ckick-clack");
+
+});
